1d8687e28b9a8bc8fdd669b79de52e849ee21996
amasum1969 <ahsan.masum@gmail.com>
2025-07-08 16:02:30 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,10 +9,6 @@ name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be: ', name_on_order)
 
 cnx = st.connection("snowflake")
-
-if not cnx.is_healthy():
-    cnx.reset()
-    
 session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
