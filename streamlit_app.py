7cbbece033081bb08708f9da87c2c4878f9c4d92
amasum1969 <ahsan.masum@gmail.com>
2025-07-08 18:59:01 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,10 +34,10 @@ my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_ord
    
 time_to_insert = st.button ('Submit Order')
     
-    if time_to_insert:
-        session.sql(my_insert_stmt).collect()
+if time_to_insert:
+    session.sql(my_insert_stmt).collect()
 
-        st.success('Your Smoothie is ordered, ' +name_on_order+'!', icon="✅")
+    st.success('Your Smoothie is ordered, ' +name_on_order+'!', icon="✅")
 
 
 
