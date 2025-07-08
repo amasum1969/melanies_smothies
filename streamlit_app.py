5706ef2a9dd011d6634848ff3b3686753f5a5caa
amasum1969 <ahsan.masum@gmail.com>
2025-07-08 13:48:02 +1000

    Update streamlit_app.py
    update cnx and session to bring down

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,6 @@ import streamlit as st
 
 from snowflake.snowpark.functions import col
 
-cnx = st.connection("snowflake")
-session = cnx.session()
-
 st.title(f":balloon: Customize Your Smoothies! :balloon:")
 st.write(
   """Chhose the fruits you want in your Smoothies!
@@ -14,6 +11,9 @@ st.write(
 
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be: ', name_on_order)
+
+cnx = st.connection("snowflake")
+session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 
