8711137622d9291976e4ce1bd1874fc56e541b5f
amasum1969 <ahsan.masum@gmail.com>
2025-07-08 15:13:29 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,8 +9,8 @@ st.write('The name on your Smoothie will be: ', name_on_order)
 
 from snowflake.snowpark.functions import col
 cnx = st.connection("snowflake")
-session = cnx.query()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+session = cnx.query(table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
